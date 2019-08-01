fe0dd97903350b0efd141d0c83ba7c8857f2aecf
feldhaus <maicon.feldhaus@gmail.com>
2019-08-01 10:42:18 +0200

    Add scale mode example and remove unused files

--- js/examples/geometry/line-intersection.js
+++ js/examples/geometry/line-intersection.js
@@ -123,18 +123,20 @@ function lineIntersection(line1Start, line1End, line2Start, line2End) {
     a = numerator1 / denominator;
     b = numerator2 / denominator;
 
-    // if we cast these graphics infinitely in both directions, they intersect here:
+    // if we cast these graphics infinitely in both directions, they intersect here
     result.x = line1Start.x + a * (line1End.x - line1Start.x);
     result.y = line1Start.y + a * (line1End.y - line1Start.y);
 
-    // if line1 is a segment and line2 is infinite, they intersect if:
+    // if line1 is a segment and line2 is infinite
     if (a > 0 && a < 1) {
         result.onLine1 = true;
     }
-    // if line2 is a segment and line1 is infinite, they intersect if:
+
+    // if line2 is a segment and line1 is infinite
     if (b > 0 && b < 1) {
         result.onLine2 = true;
     }
+
     // if line1 and line2 are segments, they intersect if both of the above are true
     return result;
 }
---
+++ js/examples/math/scale-mode.js
@@ -0,0 +1,74 @@
+// constants
+const COLOR = {
+    grey: 0x21252f,
+    pink: 0xec407a,
+    white: 0xf2f5ea,
+};
+
+// create application
+const app = new PIXI.Application({
+    backgroundColor: COLOR.grey,
+    antialias: true,
+});
+document.body.appendChild(app.view);
+
+// add background
+const background = new PIXI.Sprite();
+app.stage.addChild(background);
+background.anchor.set(0.5);
+background.position.set(app.renderer.width * 0.5, app.renderer.height * 0.5);
+
+// add graphics
+const graphics = new PIXI.Graphics();
+app.stage.addChild(graphics);
+graphics.lineStyle(3, 0xffffff);
+graphics.drawRect(-200, -150, 400, 300);
+graphics.position.set(app.renderer.width * 0.5, app.renderer.height * 0.5);
+
+// load image
+PIXI.Loader.shared
+    .add('background', './assets/images/background.jpg')
+    .load(onLoaded);
+
+// listen pointer down event
+app.renderer.plugins.interaction.on('pointerdown', switchScaleMode);
+
+function onLoaded() {
+    background.texture = PIXI.Texture.from('background');
+
+    switchScaleMode();
+}
+
+let flag = true;
+
+function switchScaleMode() {
+    background.scale.set(1);
+    let scale = 1;
+    if (flag) {
+        scale = scaleNoBorder(
+            background.width,
+            background.height,
+            graphics.width,
+            graphics.height
+        );
+    } else {
+        scale = scaleShowAll(
+            background.width,
+            background.height,
+            graphics.width,
+            graphics.height
+        );
+    }
+    background.scale.set(scale);
+    flag = !flag;
+}
+
+// the entire image fill the specified area
+function scaleNoBorder(imgWidth, imgHeight, targetWidth, targetHeight) {
+    return Math.max(targetWidth / imgWidth, targetHeight / imgHeight);
+}
+
+// the entire image be visible in the specified area and borders can appear on two sides
+function scaleShowAll(imgWidth, imgHeight, targetWidth, targetHeight) {
+    return Math.min(targetWidth / imgWidth, targetHeight / imgHeight);
+}
